aa7bfabd8b3a87a05e2461b6485694dc0ca76e4a
eriya520 <tangyadcn@gmail.com>
2026-03-05 09:58:57 -0500

    fix-issue

--- boston_weather/pipeline/data/load_tasty_bytes.sql
+++ boston_weather/pipeline/data/load_tasty_bytes.sql
@@ -65,8 +65,7 @@ CREATE OR ALTER TABLE {{env}}_tasty_bytes.raw_pos.country
    iso_country VARCHAR(2),
    city VARCHAR(16777216),
    city_population VARCHAR(16777216),
-   city_id NUMBER(19,0),
-   
+   city_id NUMBER(19,0)   
 );
 
 
